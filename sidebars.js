616bd1dc97aa2a0979499d8ce42e843e6c5e7fcc
newgeko <newgekko@gmail.com>
2026-06-03 10:24:52 +0200

    Create sidebars.js

---
+++ sidebars.js
@@ -0,0 +1,13 @@
+const sidebars = {
+  tutorialSidebar: [
+    'intro',
+    'architecture',
+    'regimes',
+    'tensions',
+    'transitions',
+    'audits',
+    'glossary'
+  ],
+};
+
+export default sidebars;
