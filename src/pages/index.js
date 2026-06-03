bb3f353fd0ac6c0c124e5eb93704c673d21f6843
newgeko <newgekko@gmail.com>
2026-06-03 10:28:50 +0200

    Create src/pages/index.js

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+export default function Home() {
+  return (
+    <Layout title="Protokin cOS">
+      <main style={{padding: '2rem'}}>
+        <h1>⚡ Protokin</h1>
+        <p>
+          Architecture d’audit des conditions de stabilisation des descriptions.
+        </p>
+
+        <p>
+          Le système ne décrit pas le monde, mais les cadres de description.
+        </p>
+      </main>
+    </Layout>
+  );
+}
